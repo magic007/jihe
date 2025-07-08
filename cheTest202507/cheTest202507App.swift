63610da3716820be2161cfbbc424d995b30e68bf
magic <730170034@qq.com>
2025-07-08 17:52:08 +0800

    Initial Commit

---
+++ cheTest202507/cheTest202507App.swift
@@ -0,0 +1,17 @@
+//
+//  cheTest202507App.swift
+//  cheTest202507
+//
+//  Created by magic on 2025/7/8.
+//
+
+import SwiftUI
+
+@main
+struct cheTest202507App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
